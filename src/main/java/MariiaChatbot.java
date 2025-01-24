a6201a6759d6036c796d0f0e53e8c53ff02dfb23
popova-mariia <tomashapopova@gmail.com>
2025-01-24 12:08:34 +0800

    added echo

--- src/main/java/MariiaChatbot.java
+++ src/main/java/MariiaChatbot.java
@@ -1,9 +1,37 @@
+import java.util.Scanner;
+
 public class MariiaChatbot {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+     
+
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm MariiaChatbot, your good buddy");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
+
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" list");
+                System.out.println("____________________________________________________________");
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(input);
+                System.out.println("____________________________________________________________");
+            }
+        }
+        
+        scanner.close();
+
+
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
