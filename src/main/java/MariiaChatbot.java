e0742d8406aa15ce68eb18509cde825ea3f36184
popova-mariia <tomashapopova@gmail.com>
2025-01-24 12:44:38 +0800

    able to add tasks

--- src/main/java/MariiaChatbot.java
+++ src/main/java/MariiaChatbot.java
@@ -1,10 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class MariiaChatbot {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> tasks = new ArrayList<>();
+
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm MariiaChatbot, your good buddy");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Here are the tasks in your list:");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i));
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks.add(input);
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + input);
+                System.out.println("____________________________________________________________");
+            }
+        }
+
+        scanner.close();
     }
 }
